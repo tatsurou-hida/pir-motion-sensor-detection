65ab8c94b251a80b1fd632d91274b173c48187cf
tatsurou-hida <tatsurou.hida@cosmicb.co.jp>
2020-03-13 15:18:09 +0900

    webhook urlは設定ファイルに持つように変更

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# coding: utf-8
 '''
 Created on 2020/03/03
 
@@ -11,6 +11,7 @@ import time
 import RPi.GPIO as GPIO
 from datetime import datetime
 import slackweb
+import configparser
 
 def wrapper(func):
     @wraps(func)
@@ -28,15 +29,19 @@ def log(message):
     print("[" + datetime.now().strftime("%Y/%m/%d %H:%M:%S") + ']: ' + message)
 
 @wrapper
-def sendToSlack(message):
+def sendToSlack(message, url):
     log("sending message to slack... message=[" + message + "]")
-    slack = slackweb.Slack(url="https://hooks.slack.com/services/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")
+    slack = slackweb.Slack(url=url)
     slack.notify(text=message)
 
 @wrapper
 def main():
     sensor_pin = 18
     sleeptime = 10
+
+    config = configparser.ConfigParser()
+    config.read('config.ini', encoding='utf-8')
+    webhook_url = config.get('Slack', 'webhook_url')
 
     GPIO.cleanup()
     GPIO.setmode(GPIO.BCM)
@@ -45,11 +50,11 @@ def main():
     try:
         log("app start")
         log("ctrl+c  :  if you want to stop the app")
-        sendToSlack("detection app started")
+        sendToSlack("detection app started", webhook_url)
         while True:
             if (GPIO.input(sensor_pin) == GPIO.HIGH):
                 log("high")
-                sendToSlack("PIR motion sensor detected")
+                sendToSlack("PIR motion sensor detected", webhook_url)
                 time.sleep(sleeptime)
 
             else:
@@ -57,7 +62,7 @@ def main():
                 time.sleep(1)
     except KeyboardInterrupt:
         log("stop")
-        sendToSlack("detection app stopped")
+        sendToSlack("detection app stopped", webhook_url)
     finally:
         log("clean up")
         GPIO.cleanup()
